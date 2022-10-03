7c44b9c90a7a85ac641778a97a0cb9e5f9eeb6fb
Monem Haque <mascot@gmail.com>
2022-10-03 16:26:02 +0000

    page created

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,13 +10,13 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-jodie/gatsby-config.js
-    siteTitle: `Jodie`,
-    siteTitleAlt: `Jodie - Gatsby Starter Portfolio`,
-    siteHeadline: `Jodie - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://jodie.lekoarts.de`,
+    siteTitle: `monemsProfile`,
+    siteTitleAlt: `monemsProfile`,
+    siteHeadline: `atm monemul haque || Portfolio`,
+    siteUrl: `https://monemsprofile.com`,
     siteDescription: `Image-heavy photography portfolio with colorful accents & customizable pages. Includes adaptive image grids powered by CSS grid and automatic image integration into projects.`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@atm monemul haque`,
   },
   plugins: [
     {
@@ -27,9 +27,9 @@ module.exports = {
           { name: `Projects`, slug: `/projects` },
           { name: `Art`, slug: `/art` },
           { name: `About`, slug: `/about` },
-          { name: `1`, slug: `/one` },
-          { name: `2`, slug: `/two` },
-          { name: `3`, slug: `/three` },
+          { name: `Photography`, slug: `/photography` },
+          { name: `Print-Graphics`, slug: `/printgraphics` },
+          { name: `Content Creation`, slug: `/contentcreation` },
         ],
       },
     },
